bd1ea48d006c5099d36acd24beb996300ea4f18a
unknown <nastena-alhovik@yandex.ru>
2022-09-12 19:36:34 +0300

    add Task 25 Homework 4

---
+++ Task_25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("a= ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b= ");
+int b = int.Parse(Console.ReadLine());
+
+//double number = Math.Pow(a,b); - первый вариант решения
+
+int number = a;
+for (int i = 1; i < b; i++)
+{
+    number = number*a;            //второй вариант решения
+}
+Console.WriteLine("Ответ: "+ number);
