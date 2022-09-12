f6cd476429d597622f359cd40994bdb2b99851cb
unknown <nastena-alhovik@yandex.ru>
2022-09-12 21:02:49 +0300

    add Task_29 Homework 4

---
+++ Task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int[] NewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 99);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+}
+int []arr = NewArray(new int[8]);
+PrintArray(arr);
