a3e0a64df6a09faf19d3fe16f47fa4280538851e
unknown <nastena-alhovik@yandex.ru>
2022-09-12 20:53:48 +0300

    add Task 27 Homework 4

---
+++ Task_27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int summ = 0;
+int n = 0;
+while (number > 0)
+{
+    n = number %10;
+    summ = summ+ n;
+    number /= 10;
+}
+Console.WriteLine("Сумма цифр равна " + summ);
